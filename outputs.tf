c32174b0eadfdd56ca76d5c4f813512f1a969e97
Selton <sl1586321@gmail.com>
2024-10-04 20:52:33 -0300

    Fixing groundwork module

--- outputs.tf
+++
@@ -1,6 +0,0 @@
-output "kubernetes_ip" {
-  value = {
-    for address in libvirt_domain.kubernetes.*.network_interface : ip => address
-  }
-  description = "Return the IP address of the domains created"
-}
